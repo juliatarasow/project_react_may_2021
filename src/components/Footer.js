9f3ca08f10b3b66a1a360602531238f5b7dc792a
Alina <alirei@gmail.com>
2021-05-12 10:27:59 +0200

    footer moved to bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./footer.css"
+
 
 export default function Footer() {
     return (
-        <div>
+        <footer>
 
             <h3>Footer</h3>
             
-        </div>
+        </footer>
     )
 }
