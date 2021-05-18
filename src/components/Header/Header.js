b4a79830cd37ab1201a3e26480a4be8f49cc85a0
Julia <juliatarasow@web.de>
2021-05-18 10:35:10 +0200

    updated styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import './Header.css';
-import Weather from '../Weather';
+import Weather from './Weather';
 import porto from '../../images/selo_2017.png';
 
 export default function Header() {
 
     return (
+
         <header>
-            <div>
+            <nav className="NavBar">
+                {/* <Link to="/">Porto</Link> */}
                 <div>
                     <img src={porto} alt="porto" style={{ height: "90px", width: "auto" }} />
                 </div>
-
+                <div className="NavLinks">
+                    <NavLink exact to="/" >Porto</NavLink>
+                    <NavLink to="/climate">Climate</NavLink>
+                    <NavLink to="/sightseeing">Sightseeing</NavLink>
+                    <NavLink to="/culture">Culture</NavLink>
+                    <NavLink to="/transport">Transport</NavLink>
+                    <NavLink to="/covid">COVID</NavLink>
+                </div>
                 <Weather />
-                
-                {/* For Anneli - coding the clock */}
                 <div className="Clock">
                     <h3>Clock</h3>
                     <div>.....</div>
-
                 </div>
-            </div>
-
-            <nav className="NavBar">
-                {/* <Link to="/">Porto</Link> */}
-                <NavLink exact to="/" >Porto</NavLink>
-                <NavLink to="/climate">Climate</NavLink>
-                <NavLink to="/sightseeing">Sightseeing</NavLink>
-                <NavLink to="/culture">Culture</NavLink>
-                <NavLink to="/transport">Transport</NavLink>
-                <NavLink to="/covid">COVID</NavLink>
-
             </nav>
+            <hr />
         </header>
+
+
     )
 }
